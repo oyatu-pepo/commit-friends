9145e526f6af6e84db2b8d3ca9a3d7209a061a31
shinji ichien <shinji11088011@gmail.com>
2018-08-25 19:38:27 +0900

    add bot

---
+++ bot/server.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require("express");
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
+
+const config = {
+  channelSecret: "b85afa07249f830b9de32aa6393493b4",
+  channelAccessToken:
+    "6U8cMeuT3Ek0hX8y/HFcibjqWdyByxrp0Plp1h1y+J0L9TMqtX2kDWdrwxi0JRnGl9GIOzBoC1rfzF9TXs8KMkOUobl4pkmMF1SPkJF4/4taBIbSN/oSj7vPE2O5WB53NgxU1+KuPGzaGDc18qBCkAdB04t89/1O/w1cDnyilFU="
+};
+
+const app = express();
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body.events);
+  Promise.all(req.body.events.map(handleEvent)).then(result =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  let replyText = "";
+  if (event.message.text === "こんにちは") {
+    replyText = "こんばんわの時間ですよ";
+  } else {
+    replyText = "hogehoge";
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: replyText
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
