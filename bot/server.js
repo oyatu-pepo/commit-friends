00ca3557c0884fc48ea19f299fc097b536e5cd3d
shinji ichien <shinji11088011@gmail.com>
2018-08-26 16:17:07 +0900

    update

--- bot/server.js
+++ bot/server.js
@@ -110,98 +110,49 @@ function handleEvent(event) {
   if (event.message.text === "はい") {
     // return replyFlex(event, "", Script.CHECK_TEMPLATE);
     return client.replyMessage(event.replyToken, {
-      type: "flex",
-      altText: "おすすめバーガー",
-      contents: {
-        type: "bubble",
-        hero: {
-          type: "image",
-          url:
-            "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          action: {
-            type: "uri",
-            uri: "https://linecorp.com"
-          }
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "md",
-          action: {
-            type: "uri",
-            uri: "https://linecorp.com"
-          },
-          contents: [
-            {
-              type: "text",
-              text: "Brown's Burger",
-              size: "xl",
-              weight: "bold"
-            },
-            {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "box",
-                  layout: "baseline",
-                  contents: [
-                    {
-                      type: "icon",
-                      url:
-                        "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
-                    },
-                    {
-                      type: "text",
-                      text: "1500円",
-                      weight: "bold",
-                      margin: "sm",
-                      flex: 0
-                    },
-                    {
-                      type: "text",
-                      text: "550kcl",
-                      size: "sm",
-                      align: "end",
-                      color: "#aaaaaa"
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              type: "text",
-              text: "Sauce, Onions, Pickles, Lettuce & Cheese",
-              wrap: true,
-              color: "#aaaaaa",
-              size: "xxs"
-            }
-          ]
-        },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "spacer",
-              size: "xxl"
-            },
-            {
-              type: "button",
-              style: "primary",
-              color: "#905c44",
-              action: {
-                type: "uri",
-                label: "購入",
-                uri: "https://linecorp.com"
-              }
-            }
-          ]
+      type: "bubble",
+      hero: {
+        type: "image",
+        url:
+          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+        action: {
+          type: "uri",
+          uri: "http://linecorp.com/"
         }
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        spacing: "md",
+        contents: [
+          {
+            type: "text",
+            text: "目標は達成できましたか？",
+            weight: "bold",
+            size: "sm"
+          },
+          {
+            type: "button",
+            style: "primary",
+            action: {
+              type: "message",
+              label: "はい",
+              text: "達成おめでとうございます"
+            }
+          },
+          {
+            type: "button",
+            style: "secondary",
+            action: {
+              type: "message",
+              label: "いいえ",
+              text: "次なら大丈夫ですよ"
+            }
+          }
+        ]
       }
     });
   }
