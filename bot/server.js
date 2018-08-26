9dabfd972d5e32d66b21bfedfcd113df601a2012
shinji ichien <shinji11088011@gmail.com>
2018-08-26 16:19:33 +0900

    update

--- bot/server.js
+++ bot/server.js
@@ -110,49 +110,53 @@ function handleEvent(event) {
   if (event.message.text === "はい") {
     // return replyFlex(event, "", Script.CHECK_TEMPLATE);
     return client.replyMessage(event.replyToken, {
-      type: "bubble",
-      hero: {
-        type: "image",
-        url:
-          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-        size: "full",
-        aspectRatio: "20:13",
-        aspectMode: "cover",
-        action: {
-          type: "uri",
-          uri: "http://linecorp.com/"
-        }
-      },
-      body: {
-        type: "box",
-        layout: "vertical",
-        spacing: "md",
-        contents: [
-          {
-            type: "text",
-            text: "目標は達成できましたか？",
-            weight: "bold",
-            size: "sm"
-          },
-          {
-            type: "button",
-            style: "primary",
-            action: {
-              type: "message",
-              label: "はい",
-              text: "達成おめでとうございます"
-            }
-          },
-          {
-            type: "button",
-            style: "secondary",
-            action: {
-              type: "message",
-              label: "いいえ",
-              text: "次なら大丈夫ですよ"
-            }
+      type: "flex",
+      altText: "おすすめバーガー",
+      contents: {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url:
+            "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+          action: {
+            type: "uri",
+            uri: "http://linecorp.com/"
           }
-        ]
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "md",
+          contents: [
+            {
+              type: "text",
+              text: "目標は達成できましたか？",
+              weight: "bold",
+              size: "sm"
+            },
+            {
+              type: "button",
+              style: "primary",
+              action: {
+                type: "message",
+                label: "はい",
+                text: "達成おめでとうございます"
+              }
+            },
+            {
+              type: "button",
+              style: "secondary",
+              action: {
+                type: "message",
+                label: "いいえ",
+                text: "次なら大丈夫ですよ"
+              }
+            }
+          ]
+        }
       }
     });
   }
