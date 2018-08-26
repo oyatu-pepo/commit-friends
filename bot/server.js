12d6c9858a437aa2ff65ef0e7a8e483afd3d2267
shinji ichien <shinji11088011@gmail.com>
2018-08-26 16:35:28 +0900

    update

--- bot/server.js
+++ bot/server.js
@@ -107,59 +107,59 @@ function handleEvent(event) {
     return;
   }
 
-  if (event.message.text === "はい") {
-    // return replyFlex(event, "", Script.CHECK_TEMPLATE);
-    return client.replyMessage(event.replyToken, {
-      type: "flex",
-      altText: "hoge",
-      contents: {
-        type: "bubble",
-        hero: {
-          type: "image",
-          url:
-            "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          action: {
-            type: "uri",
-            uri: "http://linecorp.com/"
-          }
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "md",
-          contents: [
-            {
-              type: "text",
-              text: "目標は達成できましたか？",
-              weight: "bold",
-              size: "sm"
-            },
-            {
-              type: "button",
-              style: "primary",
-              action: {
-                type: "message",
-                label: "はい",
-                text: "達成おめでとうございます"
-              }
-            },
-            {
-              type: "button",
-              style: "secondary",
-              action: {
-                type: "message",
-                label: "いいえ",
-                text: "次なら大丈夫ですよ"
-              }
-            }
-          ]
-        }
-      }
-    });
-  }
+  // if (event.message.text === "はい") {
+  //   // return replyFlex(event, "", Script.CHECK_TEMPLATE);
+  //   return client.replyMessage(event.replyToken, {
+  //     type: "flex",
+  //     altText: "hoge",
+  //     contents: {
+  //       type: "bubble",
+  //       hero: {
+  //         type: "image",
+  //         url:
+  //           "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+  //         size: "full",
+  //         aspectRatio: "20:13",
+  //         aspectMode: "cover",
+  //         action: {
+  //           type: "uri",
+  //           uri: "http://linecorp.com/"
+  //         }
+  //       },
+  //       body: {
+  //         type: "box",
+  //         layout: "vertical",
+  //         spacing: "md",
+  //         contents: [
+  //           {
+  //             type: "text",
+  //             text: "目標は達成できましたか？",
+  //             weight: "bold",
+  //             size: "sm"
+  //           },
+  //           {
+  //             type: "button",
+  //             style: "primary",
+  //             action: {
+  //               type: "message",
+  //               label: "はい",
+  //               text: "達成おめでとうございます"
+  //             }
+  //           },
+  //           {
+  //             type: "button",
+  //             style: "secondary",
+  //             action: {
+  //               type: "message",
+  //               label: "いいえ",
+  //               text: "次なら大丈夫ですよ"
+  //             }
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   });
+  // }
 
   // botに返答
   if (replyText === "") {
