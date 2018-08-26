6fcd08c5a4c55f5a5b93ba07b851408352364ee9
shinji ichien <shinji11088011@gmail.com>
2018-08-26 13:40:57 +0900

    commit

---
+++ bot/const.js
@@ -0,0 +1,21 @@
+module.exports = Object.freeze({
+  // てきとー
+  PROGRESS_STATUS_RESULT_BAD: "進捗だめです!",
+
+  // 目標登録
+  REGISTER_GOAL_OK: "目標を登録しました!",
+
+  // 進捗報告
+  ASK_PROGRESSS_STATUS: "今日は頑張れましたか? 教えてください。",
+  PROGRESS_STATUS_GOOD:
+    "さすがです！本当に素晴らしいですね。明日もこの調子でがんばっていきましょう。",
+  PROGRESS_STATUS_NOT_GOOD:
+    "あともう一歩ですね。明日もがんばりましょう。応援しています。",
+  PROGRESS_STATUS_BAD:
+    "そうだったんですね。そういう日もありますね。また明日がんばりましょう",
+
+  // 目標最終確認
+  FINAL_CHECK_GOAL: "目標設定日になりました。目標は達成できましたか?",
+  FINAL_CHECK_YES: "達成おめでとうございます！！",
+  FINAL_CHECK_NO: "次ならダイジョブですよ"
+});
