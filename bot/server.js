5fbed3fb7baed0099dd8410f02c2f1d4166b394b
shinji ichien <shinji11088011@gmail.com>
2018-08-26 16:16:11 +0900

    update

--- bot/server.js
+++ bot/server.js
@@ -108,7 +108,102 @@ function handleEvent(event) {
   }
 
   if (event.message.text === "はい") {
-    return replyTemplate(event, "", Script.CHECK_TEMPLATE);
+    // return replyFlex(event, "", Script.CHECK_TEMPLATE);
+    return client.replyMessage(event.replyToken, {
+      type: "flex",
+      altText: "おすすめバーガー",
+      contents: {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url:
+            "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+          action: {
+            type: "uri",
+            uri: "https://linecorp.com"
+          }
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "md",
+          action: {
+            type: "uri",
+            uri: "https://linecorp.com"
+          },
+          contents: [
+            {
+              type: "text",
+              text: "Brown's Burger",
+              size: "xl",
+              weight: "bold"
+            },
+            {
+              type: "box",
+              layout: "vertical",
+              spacing: "sm",
+              contents: [
+                {
+                  type: "box",
+                  layout: "baseline",
+                  contents: [
+                    {
+                      type: "icon",
+                      url:
+                        "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
+                    },
+                    {
+                      type: "text",
+                      text: "1500円",
+                      weight: "bold",
+                      margin: "sm",
+                      flex: 0
+                    },
+                    {
+                      type: "text",
+                      text: "550kcl",
+                      size: "sm",
+                      align: "end",
+                      color: "#aaaaaa"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              type: "text",
+              text: "Sauce, Onions, Pickles, Lettuce & Cheese",
+              wrap: true,
+              color: "#aaaaaa",
+              size: "xxs"
+            }
+          ]
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "spacer",
+              size: "xxl"
+            },
+            {
+              type: "button",
+              style: "primary",
+              color: "#905c44",
+              action: {
+                type: "uri",
+                label: "購入",
+                uri: "https://linecorp.com"
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 
   // botに返答
@@ -127,7 +222,7 @@ function replyMessage(event, message) {
   });
 }
 
-function replyTemplate(event, message, template) {
+function replyFlex(event, message, template) {
   return client.replyMessage(event.replyToken, {
     type: "flex",
     altText: message,
